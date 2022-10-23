25593ed180f5b86bb5a87c30981bcb7622af2067
SteelRobot <alutchak@yandex.ru>
2022-10-23 17:22:53 +0300

    5 урок, начало

--- src/lesson5/task1/Map.kt
+++ src/lesson5/task1/Map.kt
@@ -117,7 +117,7 @@ fun buildGrades(grades: Map<String, Int>): Map<Int, List<String>> {
  *   containsIn(mapOf("a" to "z"), mapOf("a" to "zee", "b" to "sweet")) -> false
  */
 fun containsIn(a: Map<String, String>, b: Map<String, String>): Boolean {
-    if (a.isEmpty() || b.isEmpty()) return true
+    if (a.isEmpty()) return true
     for ((key, value) in a) {
         if (value == b[key]) return true
     }
